df344c098d343020cf4a0c18ec67eafffc77397b
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-12-07 19:09:29 +0200

    add generate/read scripts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { PATH_DB } from "../constants/contacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+import * as fs from "node:fs/promises";
+
+const generateContacts = async (number) => {
+  const contacts = Array(number).fill(0).map(createFakeContact);
+  console.log(contacts);
+  const buffer = await fs.readFile(PATH_DB, "utf-8");
+  const data = JSON.parse(buffer);
+  return data;
+};
 
 generateContacts(5);
