622cff2da8855e4a3d75ea13a3b39c87f0c127d8
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-12-08 17:19:42 +0200

    add count script

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,6 +1,5 @@
 import { PATH_DB } from "../constants/contacts.js";
 import * as fs from "node:fs/promises";
 
-export const writeContacts = async (updatedContacts) => {
-  await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
-};
+export const writeContacts = (updatedContacts) =>
+  fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
